cfd2fe3295d52c228390942f8fbdb2c6773b7976
Matheus <matheusramossilva2003@gmail.com>
2024-12-26 19:44:25 -0300

    upload

---
+++ ArticlePortal/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ArticlePortal.Models;
+
+namespace ArticlePortal.Data
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+		public DbSet<Article> Articles { get; set; }
+	}
+}
---
+++ ArticlePortal/Models/Article.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ArticlePortal.Models
+{
+	public class Article
+	{
+		public int Id { get; set; }
+
+		[Required]
+		public string Title { get; set; }
+
+		[Required]
+		public string Content { get; set; }
+
+		public string? Author { get; set; }
+		public DateTime PublishedDate { get; set; }
+	}
+}
