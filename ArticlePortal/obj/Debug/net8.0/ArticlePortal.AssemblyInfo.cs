4a8c4a8fb4f173d555ecb42553f31aae5a95b4e0
Matheus <matheusramossilva2003@gmail.com>
2025-02-21 02:09:13 -0300

    upload

--- ArticlePortal/obj/Debug/net8.0/ArticlePortal.AssemblyInfo.cs
+++ ArticlePortal/obj/Debug/net8.0/ArticlePortal.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArticlePortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfd2fe3295d52c228390942f8fbdb2c6773b7976")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArticlePortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArticlePortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
